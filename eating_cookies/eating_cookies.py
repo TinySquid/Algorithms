fc469746cfb0ede81b5d0e1da655c73e9d575067
Michael Nunes <tinysquid.git@gmail.com>
2020-03-18 19:51:59 -0700

    Add recursive eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,16 +1,31 @@
-#!/usr/bin/python
-
 import sys
 
-# The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
-# recursive solution
+
 def eating_cookies(n, cache=None):
-  pass
+    """
+    n - int - Number of cookies in the jar
+    cache - list - memoization of cookie methods already calculated
+    """
+
+    count = 0
+
+    if n <= 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
+    return count
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print(
+            "There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+                ways=eating_cookies(num_cookies), n=num_cookies
+            )
+        )
+    else:
+        print("Usage: eating_cookies.py [num_cookies]")
+
